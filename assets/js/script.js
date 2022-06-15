9d0daefdb2037bdc71b10ca0adbee19604d3d51f
chris <christiancarmona3@gmail.com>
2022-06-14 20:00:04 -0500

    Updated rule section

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var startButtonEl = document.querySelector("#start-game");
+
+
+
+var startGame = function() {
+    console.log("hi");
+};
+startButtonEl.addEventListener("click", startGame);
+console.log(startButtonEl);
